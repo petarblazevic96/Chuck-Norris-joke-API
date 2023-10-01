3304253d190024e0602d4b1e646aa41e4e8e5480
petarblazevic96 <petarblaevi307@gmail.com>
2023-10-01 18:32:09 +0200

    Basic db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,21 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
+import { SequelizeModule } from '@nestjs/sequelize';
+
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule, 
+    UsersModule,
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      models: [],
+      //TODO transfer this to config later
+      host: "localhost",
+      port: 5432,
+      database: "chuck_norris_joke_app"
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
