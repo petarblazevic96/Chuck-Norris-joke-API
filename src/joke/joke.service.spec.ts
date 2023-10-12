c117f3d0ba7baf9c8ecafdca68b8079d7db2169e
petarblazevic96 <petarblaevi307@gmail.com>
2023-10-12 11:22:08 +0200

    Added joke service test

---
+++ src/joke/joke.service.spec.ts
@@ -0,0 +1,106 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import Bull, { Queue } from 'bull';
+import { JokeService } from './joke.service';
+import { getQueueToken } from '@nestjs/bull';
+import { MockFunctionMetadata, ModuleMocker } from 'jest-mock';
+import { UsersService } from '../users/users.service';
+import { AxiosHeaders, AxiosResponse } from 'axios';
+import { HttpService } from '@nestjs/axios';
+import { NotFoundError, of, throwError } from 'rxjs';
+import { InternalServerErrorException, NotFoundException } from '@nestjs/common';
+
+const moduleMocker = new ModuleMocker(global);
+
+describe('UsersService', () => {
+    let service: JokeService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                JokeService,
+                {
+                    provide: getQueueToken("email"),
+                    useValue: {
+                        add: jest.fn(async (name: string, data: any, opts: Bull.JobOptions) => { }),
+                        process: jest.fn()
+                    }
+                }
+            ],
+        })
+            .useMocker((token) => {
+                if (token === UsersService) {
+                    return {
+                        getUserById: jest
+                            .fn()
+                            .mockReturnValue({
+                                id: "1",
+                                email: "mail@mail.com"
+                            })
+                    }
+                }
+                if (typeof token === 'function') {
+                    const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+                    const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+                    return new Mock();
+                }
+            })
+            .compile();
+
+        service = module.get<JokeService>(JokeService);
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    it("should not fail", async () => {
+        const data = ['test'];
+
+        const response: AxiosResponse<unknown, any> =
+        {
+            status: 200,
+            statusText: "OK",
+            headers: {},
+            config: {
+                headers: new AxiosHeaders()
+            },
+            data: data
+        };
+
+        const httpSpy = jest.spyOn(service["httpService"], "get").mockReturnValue(of(response));
+
+        expect(await service.getRandomJoke("1")).toBe(undefined);
+    });
+
+    it("should throw internal server error", async () => {
+        try {
+            const httpSpy = jest.spyOn(service["httpService"], "get").mockReturnValue(throwError(() => new Error("Error")));
+            await service.getRandomJoke("1");
+        } catch (error) {
+            expect(error).toBeInstanceOf(InternalServerErrorException)
+        }
+    });
+
+    it("should throw not found exception", async () => {
+        try {
+            const data = ['test'];
+
+            const response: AxiosResponse<unknown, any> =
+            {
+                status: 200,
+                statusText: "OK",
+                headers: {},
+                config: {
+                    headers: new AxiosHeaders()
+                },
+                data: data
+            };
+
+            const httpSpy = jest.spyOn(service["httpService"], "get").mockReturnValue(of(response));
+
+            await service.getRandomJoke("wrong id");
+        } catch (error) {
+            expect(error).toBeInstanceOf(NotFoundException);
+        }
+    });
+});
--- src/joke/joke.service.ts
+++ src/joke/joke.service.ts
@@ -10,7 +10,6 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { ConfigService } from '@nestjs/config';
 import { UsersService } from '../users/users.service';
-import { CustomLogger } from '../logger/logger.service';
 import { ChuckNorrisApi } from '../config/interfaces';
 
 
